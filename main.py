972c4545023725e73a99afbc6cb974ecedc91688
Adam Jurkiewicz <a.jurkiewicz5@gmail.com>
2022-10-15 13:15:17 +0200

    Add all required methods, doesn't work yet though

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from easyAI import TwoPlayerGame, Human_Player, AI_Player, Negamax
 
 
-def getBlackPegs(target, guess):
-    """
-    Return result of single row comparison of guess and target pegs.
-
-    B (black) - peg is in correct color code and position
-    W (white) - peg is in correct color code, but in a wrong position
-    _ (none)  -
-    """
-    pegs = []
-
-
-    return pegs
-
-
 class Mastermind(TwoPlayerGame):
+
+    # def getBlackPegs(target, guess):
+    #     """
+    #     Return result of single row comparison of guess and target pegs.
+
+    #     B (black) - peg is in correct color code and position
+    #     W (white) - peg is in correct color code, but in a wrong position
+    #     _ (none)  -
+    #     """
+    #     pegs = []
+    #     return pegs
 
     def __init__(self, players):
         self.players = players
+        # self.password = ['R', 'G', 'B'] # TODO: will be inputed by human player
         self.password = ['R']
         self.guess = []
+        self.scoreBlack = []
+        self.scoreWhite = []
+        self.rounds = 10
         # self.password = ['R', 'G', 'B', 'X']
 
 
-    def possible_moves(self): return ["R", "G", "B", "Y"]
-    def make_move(self, move): self.guess
-
+    # list of 3-letter moves 
+    def possible_moves(self): [combination for combination in self.password]
+    def make_move(self, move):
+        self.rounds -= 1
+    def is_over(self):
+        return (self.rounds == 0)
+    def show(self):
+        print("Guess: ", self.guess)
 
 class Board():
     columns = 4
@@ -36,8 +42,8 @@ class Board():
 
 
 if __name__ == '__main__':
-    print(getBlackPegs(['R', 'G', 'B', 'X'], ['R', 'R', 'G', 'Y']))
-    Mastermind()
+    # print(getBlackPegs(['R', 'G', 'B', 'X'], ['R', 'R', 'G', 'Y']))
+    Mastermind([AI_Player(Negamax(9))])
 
 
 
